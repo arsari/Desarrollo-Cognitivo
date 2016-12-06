3faa45ba8191bf0af0b1f6de380de76031dfb902
ARSARI <asantiago@arsari.com>
2016-12-06 19:19:36 -0400

    Project Setup

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,33 +1,110 @@
+/*
+ *  Codetrotters Intro to Web Development Fall 2016
+ *  JS File Final Project
+ *  Arturo Santiago-Rivera
+ */
+
 $(document).ready(function() {
-  // Init
-  $.get('home.html', function(data) {
-    $('.container').html(data);
+
+// section-panels toggle
+  $('#welcome-bar').click(function() {
+    $('#welcome-bar').addClass('active');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-3').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#section-1').click(function() {
+    $('#section-1').addClass('active');
+    $('#welcome').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-3').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#section-2').click(function() {
+    $('#section-2').addClass('active');
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-3').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#section-3').click(function() {
+    $('#section-3').addClass('active');
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#section-4').click(function() {
+    $('#section-4').addClass('active');
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-3').removeClass('in');
   });
 
-  // Links
-  $('#home').on('click', function() {
-    $.get('home.html', function(data) {
-      $('.container').html(data);
-    });
+// carousel-caption onClick location
+  $('.carousel-caption').click(function() {
+    document.location = '#welcome-bar';
   });
 
-  $('#about').on('click', function() {
-    $.get('about.html', function(data) {
-      $('.container').html(data);
-    });
+// myFooter etapas links onClick location
+  $('#sl-1').click(function() {
+    document.location = '#section-1';
+    $('#welcome').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-3').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#sl-2').click(function() {
+    document.location = '#section-2';
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-3').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#sl-3').click(function() {
+    document.location = '#section-3';
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-4').removeClass('in');
+  });
+  $('#sl-4').click(function() {
+    document.location = '#section-4';
+    $('#welcome').removeClass('in');
+    $('#collapse-1').removeClass('in');
+    $('#collapse-2').removeClass('in');
+    $('#collapse-3').removeClass('in');
   });
 
-  // Events that need to be listened upon change
-  $('.container').on('click', '.animate', function() {
-    $('img').animate({
-      left: "+=500"
-    }, 2000, function() {
-      $(this).animate({
-        opacity: 0,
-        bottom: "+=500"
-      }, 2000, function() {
-        $('h1').text('Complete');
-      });
+// popover plug-in
+  $(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+
+// tooltip plug-in
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+// .back_to_top floating button
+  $(function() {
+    var offset = 300;
+    var duration = 500;
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back_to_top').fadeIn(duration);
+        } else {
+            $('.back_to_top').fadeOut(duration);
+        }
     });
+
+    $('.back_to_top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
   });
 });
